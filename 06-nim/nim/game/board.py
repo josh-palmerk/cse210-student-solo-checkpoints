13574a14d0eaf4c64e10e43bdefd38b6ed9ab603
Josh Palmer <josh_palmerk@byui.edu>
2021-10-19 19:48:54 -0600

    finished nim

--- 06-nim/nim/game/board.py
+++ 06-nim/nim/game/board.py
@@ -1,3 +1,35 @@
 import random
 
 # TODO: Define the Board class here
+
+class Board():
+    """
+    
+    """
+    def __init__(self):
+        """
+        Class initializer
+        """
+        self._piles = [] # list of nums
+        self._prepare()
+
+    def apply(self, move):
+        self._piles[move.get_pile()] = max(0, self._piles[move.get_pile()] - move.get_stones())
+
+
+    def is_empty(self):
+        if sum(self._piles) == 0:
+            return True
+        else:
+            return False
+
+    def to_string(self):
+        string_list = []
+        for i in range(len(self._piles)):
+            string_list.append(f"{i}:  " + ("O " * self._piles[i]))
+        string = "\n".join(string_list)
+        return string
+
+    def _prepare(self):
+        for i in range(random.randint(2, 5)):
+            self._piles.append(random.randint(1,9))
